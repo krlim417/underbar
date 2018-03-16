813c8c77aca2de91bb6c2082dd944ba9595e15ab
krlim417 <krlim417@gmail.com>
2018-03-16 13:59:39 -0700

    Complete extend function

--- src/underbar.js
+++ src/underbar.js
@@ -260,6 +260,7 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    return Object.assign(obj, ...arguments);
   };
 
   // Like extend, but doesn't ever overwrite a key that already
